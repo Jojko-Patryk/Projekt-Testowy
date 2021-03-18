f0d966927e3bbdcd9eea545e7cdb4d63c5d7d67f
Jojko-Patryk <jojko.patryk0@gmail.com>
2021-03-18 18:39:34 +0100

    Testowy Commit

--- index.php
+++ index.php
@@ -7,9 +7,38 @@
     <title>Document</title>
 </head>
 <body>
+<form action="insert.php" method="post">
+        <input type="text" name="imie" placeholder="Imię"></br>
+        <input type="number" name="dzial" placeholder="Dział"></br>
+        <input type="number" name="zarobki" placeholder="Zarobki"></br>
+        <input type="date" name="data_urodzenia" placeholder="Data Urodzenia"></br>
+        <input type="submit" value="Dodaj">
+    </form>
+
 <?
 
+echo("Data i czas : ".date("Y-m-d h:i:sa", $d));
 
+
+
+require("connect.php");
+    $sql = ('SELECT * FROM pracownicy');
+    echo("<h2>Pracownicy");
+    echo("<h3>".$sql."</h3>");
+        $result = $conn->query($sql);
+            echo("<table border=1>");
+            echo("<th>Imie</th>");
+            echo("<th>Zarobki</th>");
+            echo("<th>Data_Urodzenia</th>");
+            echo("<th>Dział</th>");
+        while($row=$result->fetch_assoc()){ 
+            echo("<tr>");
+                echo("<td>".$row["imie"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["dzial"]."</td>");
+
+            echo("</tr>");
+        }
+
+        echo("</table>");
 
 ?>
 </body>
